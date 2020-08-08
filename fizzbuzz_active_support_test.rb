39d107af770c74a70b1d79add80a7c0081770945
masuyama13 <masterxwebm13@gmail.com>
2020-08-08 10:45:23 +0900

    Fix test codes

--- fizzbuzz_active_support_test.rb
+++ fizzbuzz_active_support_test.rb
@@ -3,27 +3,23 @@ require "active_support/all"
 require "./fizzbuzz"
 
 class FizzbuzzTest < ActiveSupport::TestCase
-  def setup
-    @fb = Fizzbuzz.new
-  end
-
   def test_1を渡すと文字列1を返す
-    assert_equal "1", @fb.fizzbuzz(1)
+    assert_equal "1", Fizzbuzz.run(1)
   end
 
   def test_3を渡すと文字列Fizzを返す
-    assert_equal "Fizz", @fb.fizzbuzz(3)
+    assert_equal "Fizz", Fizzbuzz.run(3)
   end
 
   def test_5を渡すと文字列Buzzを返す
-    assert_equal "Buzz", @fb.fizzbuzz(5)
+    assert_equal "Buzz", Fizzbuzz.run(5)
   end
 
   def test_15を渡すと文字列Buzzを返さない
-    assert_not_equal "Buzz", @fb.fizzbuzz(15)
+    assert_not_equal "Buzz", Fizzbuzz.run(15)
   end
 
   def test_15を渡すと文字列FizzBuzzを返す
-    assert_equal "FizzBuzz", @fb.fizzbuzz(15)
+    assert_equal "FizzBuzz", Fizzbuzz.run(15)
   end
 end
--- fizzbuzz_minitest.rb
+++ fizzbuzz_minitest.rb
@@ -2,27 +2,23 @@ require "minitest/autorun"
 require "./fizzbuzz"
 
 class FizzbuzzTest < Minitest::Test
-  def setup
-    @fb = Fizzbuzz.new
-  end
-
   def test_1を渡すと文字列1を返す
-    assert_equal "1", @fb.fizzbuzz(1)
+    assert_equal "1", Fizzbuzz.run(1)
   end
 
   def test_3を渡すと文字列Fizzを返す
-    assert_equal "Fizz", @fb.fizzbuzz(3)
+    assert_equal "Fizz", Fizzbuzz.run(3)
   end
 
   def test_5を渡すと文字列Buzzを返す
-    assert_equal "Buzz", @fb.fizzbuzz(5)
+    assert_equal "Buzz", Fizzbuzz.run(5)
   end
 
   def test_15を渡すと文字列Buzzを返さない
-    refute_equal "Buzz", @fb.fizzbuzz(15)
+    refute_equal "Buzz", Fizzbuzz.run(15)
   end
 
   def test_15を渡すと文字列FizzBuzzを返す
-    assert_equal "FizzBuzz", @fb.fizzbuzz(15)
+    assert_equal "FizzBuzz", Fizzbuzz.run(15)
   end
 end
